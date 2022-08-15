5b26445c4758a10c5481c5a2b09fa5b44320c4f7
Bishal <bishal1831047@gmail.com>
2022-08-15 16:55:34 +0600

    second

---
+++ js/event.js
@@ -0,0 +1,11 @@
+function red(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+const first = document.getElementById('yellow');
+
+first.onclick = yellowColor;
+
+function yellowColor(){
+    document.body.style.backgroundColor = 'yellow';
+}
