ee34cc15e8f881dd48c7e13cc189c46abc8706ae
Bishal <bishal1831047@gmail.com>
2022-08-15 17:26:28 +0600

    second

--- js/event.js
+++ js/event.js
@@ -9,3 +9,28 @@ first.onclick = yellowColor;
 function yellowColor(){
     document.body.style.backgroundColor = 'yellow';
 }
+//handle with anonymous function
+
+const makeGreen = document.getElementById('green');
+
+makeGreen.onclick = function (){
+  document.body.style.backgroundColor = 'green';
+}
+
+//handle with event listener
+
+const makeCoral = document.getElementById('make-coral');
+
+makeCoral.addEventListener('click',makeC);
+
+function makeC (){
+    document.body.style.backgroundColor = 'coral';
+}
+
+//handle with event lister in two line
+
+const makeCyan = document.getElementById('make-cyan');
+
+makeCyan.addEventListener('click',function makeCy(){
+    document.body.style.backgroundColor = 'cyan';
+})
